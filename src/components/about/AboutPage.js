5e2e16995c7bd71f91353d6e463024809ab97c8a
Amiinoz <momagan@gmail.com>
2020-04-22 16:24:23 -0600

    Creating first pages

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AboutPage = () => {
+  <div>
+    <h2>About</h2>
+    <p>This app uses React, Redux and many other Libraries.</p>
+  </div>;
+};
+
+export default AboutPage;
